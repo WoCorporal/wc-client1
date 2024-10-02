c9ed5614f15ff8054ee154d9672bd28f6c9a8c57
joel-CM <joelchavezm19@gmail.com>
2024-10-02 00:11:39 -0300

    refactor(mongo connection file): refactor connection and env variables

--- src/app/_db/conexion.ts
+++ src/app/_db/conexion.ts
@@ -1,8 +1,25 @@
+/* This is a database connection function*/
 import mongoose from "mongoose";
+import { MONGO_URL } from "@/config/env_d";
 
-export default function connect() {
-  mongoose
-    .connect(process.env.MONGO_URL!)
-    .then(() => console.log("db connected!"))
-    .catch((error) => console.log(error));
+type IConnection = { isConnected?: number };
+const connection: IConnection = {}; /* creating connection object*/
+
+async function dbConnect() {
+  /* check if we have connection to our databse*/
+  try {
+    if (connection.isConnected) {
+      console.log("Ya existe una conexion, continuamos en esa!");
+      return;
+    }
+
+    /* connecting to our database */
+    const db = await mongoose.connect(MONGO_URL, {});
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log("|> Mongo DB conectado ");
+  } catch (e) {
+    console.log("Error", e);
+  }
 }
+export default dbConnect;
