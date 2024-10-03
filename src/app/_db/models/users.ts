b18bb9e0359e650bfce40ee4f89bc111a7441e71
joel-CM <joelchavezm19@gmail.com>
2024-10-02 23:22:18 -0300

    feat(db models folder): create exercise model
    deleted test model (users)

--- src/app/_db/models/users.ts
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const schema = new mongoose.Schema({
-  name: String,
-  lastName: String,
-});
-
-export default mongoose.models.User || mongoose.model("User", schema);
