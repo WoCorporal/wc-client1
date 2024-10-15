938b0dbc3e70abfe6d03917378ba0fc2a79c8cad
JulioIGonzalez <lagafish2020@gmail.com>
2024-10-14 22:49:57 -0300

    actualizando rama feature/api/consultar-ejercicios

--- src/app/api/exercise/route.ts
+++ src/app/api/exercise/route.ts
@@ -1,40 +1,71 @@
 import mongoose from "mongoose";
 import exercise from "@/app/_db/models/exercises";
 import { exerciseSchema } from "@/utils/schemas/exercise";
-import { MONGODB_DUPLICATE_KEY_ERROR } from "@/utils/codeErrors/mongodb";
+import { MONGODB_DUPLICATE_KEY_ERROR } from "@/utils/codeErrors/mongodb"
+import dbConnect from "@/app/_db/conexion";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const body = await req.json();
-  const valdiation = exerciseSchema.safeParse(body);
+  const validation = exerciseSchema.safeParse(body);
 
-  if (!valdiation.success) {
-    console.log(valdiation.error);
-    return Response.json(
-      { message: valdiation.error.issues[0].message },
+  if (!validation.success) {
+    console.log(validation.error);
+    return NextResponse.json(
+      { message: validation.error.issues[0].message },
       { status: 400 }
     );
   }
 
   try {
-    const savedExercise = await exercise.create(valdiation.data);
-    return Response.json({ data: { id: savedExercise.id } }, { status: 201 });
+    const savedExercise = await exercise.create(validation.data);
+    return NextResponse.json({ data: { id: savedExercise.id } }, { status: 201 });
   } catch (error) {
     if ((error as { code: number }).code === MONGODB_DUPLICATE_KEY_ERROR) {
-      return Response.json({ message: "duplicate key error" }, { status: 400 });
+      return NextResponse.json({ message: "duplicate key error" }, { status: 400 });
     }
     if (error instanceof mongoose.Error.ValidationError) {
       const fieldError = error.errors[Object.keys(error.errors)[0]];
       if (fieldError.kind === "required") {
-        return Response.json(
+        return NextResponse.json(
           { message: `${fieldError.path} is required` },
           { status: 400 }
         );
       }
     }
     console.log(error);
-    return Response.json(
+    return NextResponse.json(
       { message: "Internal server error." },
       { status: 500 }
     );
+  }
+}
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const category = searchParams.get("category");
+  const difficulty = searchParams.get("difficulty");
+
+  try {
+    await dbConnect();
+
+    const filter: {
+      category?: string;
+      difficulty?: number;
+    } = {}; 
+
+    if (category) {
+      filter.category = category.toLowerCase();
+    }
+
+    if (difficulty) {
+      filter.difficulty = parseInt(difficulty);
+    }
+
+    const exercises = await exercise.find(filter);
+    return NextResponse.json(exercises);
+  } catch (error) {
+    console.error(error); // Registrar el error
+    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
   }
 }
